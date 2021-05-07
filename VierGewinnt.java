bb270fd12c18f9d10f473c3f84b3b98e2d1213de
Tobias <tobias.ros@outlook.de>
2021-05-07 10:47:56 +0200

    yo

---
+++ VierGewinnt.java
@@ -0,0 +1,24 @@
+
+/**
+ * Beschreiben Sie hier die Klasse vierGewinnt.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class VierGewinnt
+{
+    char[][] feld;
+    
+    /**
+     * Konstruktor für Objekte der Klasse vierGewinnt
+     */
+    public VierGewinnt()
+    {
+        feld = new char[6][7];
+    }
+
+    public void chipPlatzieren(int reihe)
+    {
+        
+    }
+}
