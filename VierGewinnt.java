27a9f7498d91374b078b9a598a03d423d2ceb08a
Tobias <tobias.ros@outlook.de>
2021-05-07 11:25:48 +0200

    gh

--- VierGewinnt.java
+++ VierGewinnt.java
@@ -8,6 +8,8 @@
 public class VierGewinnt
 {
     char[][] feld;
+    int turn;
+    boolean spieler;
     
     /**
      * Konstruktor für Objekte der Klasse vierGewinnt
@@ -17,8 +19,21 @@ public class VierGewinnt
         feld = new char[6][7];
     }
 
-    public void chipPlatzieren(int reihe)
+    public void chipPlatzieren(int spalte)
     {
         
+    }
+    
+    public int chipsInSpalte(int spalte)
+    {
+        int count = 0;
+        for(int i=0; i<=6; i++)
+        {
+            if(getChar(feld[i][spalte]) )
+            {
+                count++;
+            }
+        }
+        return count;
     }
 }
