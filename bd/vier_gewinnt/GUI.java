07f395e1c69d666d017e1d4f029e68d6492c48a0
Malte <maltekindhaeuser@gmail.com>
2021-06-01 19:13:12 +0200

    Bei Herrausforderung (popup) wird bereits eigenes Spielfeld geöffnet To do: Darf eigentlich nur geschehen wenn die Herrausforderung angenommen wurde.

--- bd/vier_gewinnt/GUI.java
+++ bd/vier_gewinnt/GUI.java
@@ -87,6 +87,7 @@ public class GUI extends JFrame
         {
             public void actionPerformed(ActionEvent e)
             {
+                Main.acceptConnection();
                 popup(tfMitspielerIP.getText());
             }
         });
